9ba30ce4a9b8f01220c745e19c20741d81fb0bb5
Bill Brown <bill.a.brown90@gmail.com>
2023-01-22 12:06:35 -0800

    commit tree baceb9b5180f3007a7027c971bd2e1df3f831728

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solutions in this file
+
+function writeCards(names,eventName) {
+    for (let i = 0; i < names.length; i++) {
+       arrayMessage[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+    }
+  return arrayMessage
+}
+
+const names = ["Charlie", "Samip", "Ali"];
+const eventName = ["birthday"];
+const arrayMessage = []
+
+
+function countDown() {
+    let n = 10;
+    while (n > -1) {
+        console.log(n--)
+    }
+}
+
+
+
+
+
+// examples
+
+// for (let age = 30; age < 40; age ++) {
+//     console.log (`I'm ${age} years old. Happy birthday to me!`);
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGift(gift) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//     return gifts
+// }
+
+// wrapGift(gifts)
