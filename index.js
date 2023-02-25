7d2b0fe0fc918de016faeb379faf353e38591761
Bill Brown <bill.a.brown90@gmail.com>
2023-02-25 11:54:09 -0800

    looping using for and while

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ["Charlie", "Bill", "Frank"]
+
+
+function writeCards(names,eventName) {
+    let messageArray = [""];
+    for (let i =0; i < names.length; i++ ) {
+    messageArray[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+    }
+    return messageArray
+}
+
+//write a countdown WHILE function
+
+function countDown (number) {
+    
+    while (number > -1) {
+        console.log(number--)
+    }
+}
